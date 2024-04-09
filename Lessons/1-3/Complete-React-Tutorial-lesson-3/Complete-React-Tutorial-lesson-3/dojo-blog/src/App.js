f7a9e219d0f15c388e487a33776e2a79603c5b6e
JeevanSawalkar <sawalkarjeevan@gmail.com>
2024-04-09 02:47:03 -0400

    after 4th lesson

---
+++ Lessons/1-3/Complete-React-Tutorial-lesson-3/Complete-React-Tutorial-lesson-3/dojo-blog/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>App Component</h1>
+        <h2>App Component</h2>
+        <h3>App Component</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
